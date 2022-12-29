2713ddd86654c712e44bce6abeba820ad42a879a
UweK17 <uwekremer1971@gmx.de>
2022-12-29 11:06:22 +0100

    versuch mit useContext

--- src/components/Hangar.js
+++ src/components/Hangar.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import {Route, Routes, useNavigate} from 'react-router-dom';
 
-const Hangar = ({route, navigation}) => {
-  const {p1N, p2N} = route.params;
+const Hangar = () => {
   return (
     <div>
-      <div>Hello {p1N}</div>
-      <div>Hello {p2N}</div>
+      <div>Hello</div>
+      <div>Hello</div>
     </div>
   )
 }
+
 
 export default Hangar
